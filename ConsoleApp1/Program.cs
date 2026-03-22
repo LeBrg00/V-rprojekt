b8087618169b7e329c466c27df02e2d2af2a5a62
Leo Berg <leo.berg.8@gmail.com>
2026-03-22 22:11:03 +0100

    sdsd

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,15 +1,20 @@
-﻿using System.Net.Mail;
+﻿
 using System.Numerics;
 using Raylib_cs;
 Raylib.InitWindow(800, 600, "Waa");
 Raylib.SetTargetFPS(60);
 int day = 0;
-int playerLevel= 1;
 int currency=0;
 int gameState =0;
-int selectAction=1;
+bool playerturn=true;
+int selectAction1=1;
+int selectAction2=1;
+int skill=0;
+int maxPlayerHealth=100;
+int playerHealth = maxPlayerHealth;
+List<string> skills=["","","","",""] ;
 Color blackHalfTransparent = new(0, 0, 0, 128);
-static void optionboxes(List<Vector2> options,List<string> optionsText, int textSize, Color c){       
+static void optionBoxes(List<Vector2> options,List<string> optionsText, int textSize, Color c){ // draws black boxes with text inside      
     for (int i = 0; i < options.Count; i++)
         {
             Raylib.DrawRectangleV(options[i],new Vector2(textSize*5,textSize*2),c);
@@ -17,7 +22,33 @@ static void optionboxes(List<Vector2> options,List<string> optionsText, int text
             Raylib.DrawText(optionsText[i],(int)options[i].X+10, (int)options[i].Y+15,textSize,Color.White);
         }
 }
-
+static int select(int action, int maxAction,int active,Vector2 position, int distance,int size)//apply´s a yellow overlay on the edge of a box and outputs a int
+    {
+        if (Raylib.IsKeyPressed(KeyboardKey.S)&&active==0)
+        {
+           action++; 
+        }
+        if (Raylib.IsKeyPressed(KeyboardKey.W)&&active==0)
+        {
+           action--; 
+        }
+        if(action>maxAction){action=1;}
+        if(action<1){action=maxAction;}
+        Raylib.DrawRectangleLines((int)position.X,distance*action+(int)position.Y,size*5/2,size,Color.Yellow);
+        return action;
+    }
+static int attack(int basedmg)
+{
+    return basedmg;
+}
+static void bar(int measurement,int maxMeasurement,string measurementName,int textsize, Vector2 position,Color boxColor, Color barColor)// draws a box with a 
+{
+    Raylib.DrawRectangleV(position,new Vector2(measurementName.Length*textsize/4*3,textsize+4),boxColor);
+    Raylib.DrawText(measurementName,(int)position.X+4,(int)position.Y+2,textsize,Color.White);
+    string measurementLengt= measurement.ToString();
+    Raylib.DrawRectangleV(new Vector2((int)position.X+measurementName.Length*textsize/4*3,(int)position.Y),new Vector2(measurementLengt.Length*textsize/4*3,textsize+4),boxColor);
+    Raylib.DrawText(measurementLengt,4+(int)position.X+measurementName.Length*textsize/4*3,(int)position.Y+2,textsize,Color.White);
+}
 while(!Raylib.WindowShouldClose())
 {
     // menu
@@ -28,7 +59,7 @@ while(!Raylib.WindowShouldClose())
 
         List<Vector2> option = [new Vector2(340,100),new Vector2(100,270),new Vector2(560,270),new Vector2(340,440),];
         List<string> optionsTex= ["Arena(W)", "skills(A)", "shop(D)", "Rest(S)"];
-        optionboxes(option, optionsTex, 28, blackHalfTransparent);
+        optionBoxes(option, optionsTex, 28, blackHalfTransparent);
         if (Raylib.IsKeyPressed(KeyboardKey.W))
         {
             gameState=1;
@@ -48,34 +79,61 @@ while(!Raylib.WindowShouldClose())
         Raylib.EndDrawing();
     }
     //arena
-    
+        //your turn 
     if (gameState == 1)
     {
         Raylib.BeginDrawing();
         Raylib.ClearBackground(Color.White);
-        optionboxes(
+        bar(
+            playerHealth, maxPlayerHealth, "HP", 40, new Vector2(30,550), blackHalfTransparent, Color.Red
+        );
+        if (playerturn==true){
+        optionBoxes(
             [new Vector2(30,300),new Vector2(30,350),new Vector2(30,400),new Vector2(30,450) ],
             ["Attack","Skill","Items","Defend"],
             20,
             blackHalfTransparent
         );
-        if (Raylib.IsKeyPressed(KeyboardKey.S))
+        selectAction1=select(
+        selectAction1,4,skill,
+        new Vector2(30,250),50,40
+        );
+        // Raylib.DrawRectangleLines(30,50*select+250,100,40,Color.Yellow);
+        //simple attack
+        if (Raylib.IsKeyPressed(KeyboardKey.D))
         {
-           selectAction++; 
+            if(selectAction1==1){skill=1;}
+            if(selectAction1==2){skill=2;}
+            if(selectAction1==3){skill=3;}
+            if(selectAction1==4){skill=4;}
         }
-        if (Raylib.IsKeyPressed(KeyboardKey.W))
+        //skills
+        if (skill == 2)
         {
-           selectAction--; 
+            optionBoxes(
+            [new Vector2(140,250),new Vector2(140,290),new Vector2(140,330),new Vector2(140,370), new Vector2(140,410) ],
+            [skills[1],skills[2],skills[3],skills[4],skills[5]],
+            18,
+            blackHalfTransparent
+        );
+        selectAction2=select(
+        selectAction2,5,0,
+        new Vector2(140,210),40,36
+        );
+        if(Raylib.IsKeyPressed(KeyboardKey.A)){skill=0;}
         }
-        if(selectAction>4){selectAction=1;}
-            //your turn 
-                //simple attack
-                //skills
+    }
                     //skill menu
                 //defend
             // enemy turn
                 //chooses enemytype       
         Raylib.EndDrawing();
+    }
+    if (gameState == 4)
+    {
+        playerHealth=maxPlayerHealth;
+        day++;
+        gameState=0;
     }
         //shop
             //shop menu
